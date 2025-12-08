c45ac0d83df3a9fc07b883dbeee2c06b652f1c1a
Davidoar15 <davidoar15@gmail.com>
2025-12-08 11:16:21 -0300

    corrections-3_chall

--- 4_logic/3_find_first_sum.py
+++ 4_logic/3_find_first_sum.py
@@ -8,17 +8,16 @@ find_first_sum(nums, goal)  # [2, 3]
 """
 
 def find_first_sum(nums, goal) -> list[int] | None: 
-    for i, n1 in enumerate(nums):
-        for j, n2 in enumerate(nums):
-            if meet_condition(n1, n2, goal): return [i, j]
-            else: continue
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == goal: return [i, j]
     return None
 
-def meet_condition(num1: int, num2: int, goal: int) -> bool: 
-    """
-    Verifies that both numbers are distinct and their sum is equal goal
-    """
-    return num1 != num2 and num1 + num2 == goal
+# def meet_condition(num1: int, num2: int, goal: int) -> bool: 
+#     """
+#     Verifies that both numbers are distinct and their sum is equal goal
+#     """
+#     return num1 != num2 and num1 + num2 == goal
 
 print(find_first_sum([4, 5, 6, 2], 8))
 print(find_first_sum([4, 6, 6, 2], 11))
